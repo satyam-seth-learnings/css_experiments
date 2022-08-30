8989ef1603cd6880c7cb42724ae7bf9d005e94bc
satyam-seth <satyam1998.1998@gmail.com>
2022-08-31 00:29:06 +0530

    add read more for long chat messages

--- Expended input message field for chat/custom.js
+++ Expended input message field for chat/custom.js
@@ -1,22 +1,90 @@
+function appendMessage(message, type) {
+    const messageElement = document.createElement('p');
+    messageElement.innerText = message;
+    messageElement.classList.add(`${type === 'sent' ? 'sent' : 'received'}-message`);
+
+    const charLimit = 100; // if message length is grater than charLimit then show read more button
+
+    if (message.length > charLimit) {
+        const readMoreBtn = document.createElement('span');
+        readMoreBtn.innerText = 'read more';
+        readMoreBtn.classList.add('read-more-btn');
+
+        const dots = document.createElement('span');
+        dots.innerText = '... ';
+
+        const hiddenText = document.createElement('span');
+        hiddenText.classList.add('hidden');
+        hiddenText.innerText = message.slice(charLimit, -1);
+
+        // read more button click handler
+        readMoreBtn.addEventListener('click', () => {
+            readMoreBtn.remove();
+            dots.remove();
+            hiddenText.classList.remove('hidden');
+        });
+
+        messageElement.innerText = message.slice(0, charLimit);
+        messageElement.appendChild(dots);
+        messageElement.appendChild(readMoreBtn);
+        messageElement.appendChild(hiddenText);
+    } else {
+        messageElement.innerHTML = message;
+    }
+
+    document.querySelector('.chat-container').appendChild(messageElement);
+}
+
+function handleSubmit() {
+    console.log('submit');
+
+    const input = document.querySelector('.message-input');
+    const message = input.innerText.trim();
+    appendMessage(message, 'sent');
+    input.innerText = '';
+
+    setTimeout(() => {
+        appendMessage(message, 'received');
+    }, 1500);
+}
+
+function disableSendButton() {
+    const sendButton = document.querySelector('.send-button');
+    sendButton.classList.remove('active-button');
+    sendButton.disabled = true;
+}
+
+function activateSendButton() {
+    const sendButton = document.querySelector('.send-button');
+    sendButton.classList.add('active-button');
+    sendButton.disabled = false;
+}
+
 window.onload = () => {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (e) => {
+    document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault();
-        console.log('submit');
-        const input = document.querySelector('.message-input');
-        const message = input.innerText;
-        input.innerText = '';
+        if (e.target.innerText.trim() !== '') {
+            handleSubmit();
+            disableSendButton();
+        }
+    });
 
-        const messageElement = document.createElement('p');
-        messageElement.innerText = message;
-        messageElement.classList.add('sent-message');
-        document.querySelector('.chat-container').appendChild(messageElement);
+    // Add EventListener on input to Activate send button when user typing message
+    document.querySelector('.message-input').addEventListener('input', (e) => {
+        if (e.target.innerText.trim() === '') {
+            disableSendButton();
+        } else {
+            activateSendButton();
+        }
+    });
 
-        setTimeout(() => {
-            const messageElement = document.createElement('p');
-            messageElement.innerText = message;
-            messageElement.classList.add('received-message');
-            document.querySelector('.chat-container').appendChild(messageElement);
-        }, 1500);
+    document.querySelector('.message-input').addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            if (e.target.innerText.trim() !== '') {
+                handleSubmit();
+                disableSendButton();
+            }
+        }
     });
 };
